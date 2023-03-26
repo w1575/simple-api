cd8ea02dba0eaf413e2ffeb0319269f281c3893c
w <dbludev@ya.ru>
2023-03-27 00:07:41 +0500

    init

---
+++ first-start.sh
@@ -0,0 +1,13 @@
+ docker run --rm \
+ -u "$(id -u):$(id -g)" \
+ -v $(pwd):/opt \
+ -w /opt \
+ laravelsail/php81-composer:latest \
+ composer install --ignore-platform-reqs
+
+SAIL='./vendor/bin/sail'
+
+echo $SAIL
+$SAIL up -d
+
+$SAIL artisan migrate
