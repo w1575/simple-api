395f36e6990536fb27469ccc6fa48a9a2937dd36
w <dbludev@ya.ru>
2023-04-18 02:23:45 +0500

    Добавлены файлы тз readme + в first-start.sh теперь присутствует аргумен --build

--- first-start.sh
+++ first-start.sh
@@ -1,14 +1,14 @@
  docker run --rm \
  -u "$(id -u):$(id -g)" \
- -v $(pwd):/opt \
+ -v "$(pwd):/opt" \
  -w /opt \
- laravelsail/php81-composer:latest \
+ laravelsail/php82-composer:latest \
  composer install --ignore-platform-reqs
 
 SAIL='./vendor/bin/sail'
 
 echo $SAIL
-$SAIL up -d
+$SAIL up -d --build
 
 $SAIL artisan migrate
 $SAIL artisan test
